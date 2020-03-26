8a286e66eed3386f6dab576d92a0159dfc679131
Sabeenah O <45824984+sabbiebaz@users.noreply.github.com>
2020-03-26 03:20:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+    let fullNames = "Ogunsanya Sabina Odegua";
+    console.log("My Name is " + fullNames )
+    document.write("<br>");
+	
+const myCourses = ["html", "css", "javascript","C#"];
+console.log("My Courses are :")
+myCourses.forEach((i) => {
+    console.log(i)
+});
+for ( i = 1; i < 200; i++) {
+    if (i % 2 == 0){
+    
+  console.log(i);
+
+    }
+}
